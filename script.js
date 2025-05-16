944c0d0345497fe45bba0082e8a956418202fa59
guerreiro15 <143714753+guerreiro15@users.noreply.github.com>
2025-05-16 08:10:32 -0300

    missão

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "afirmação"
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
+                afirmacao:"Quis saber como usar IA no seu dia a dia."
             }
         ]
     },
